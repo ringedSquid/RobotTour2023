5c2fa2078ee2f4446d770e54423dbe3f1c583d40
pilot <thanoscarthanoscar420420@gmail.com>
2024-02-04 14:48:22 -0500

    regionals push

--- Clinton/v3/PATH0.h
+++ Clinton/v3/PATH0.h
@@ -1,9 +1,9 @@
 #include "PATHS.h"
-#define PATH0_SIZE 3
+#define PATH0_SIZE 8
 
-#define PATH0 {EDGE_A0, P_D0, P_D3}
+#define PATH0 {EDGE_2, P_A2, P_A3, P_B3, P_B2, P_D2, P_D0, P_B0}
 //Starting point and heading
 
-#define TARGET_TIME 30 //offset1
+#define TARGET_TIME 62 //offset1
 
 //EDGE_C0, P_B2, P_B1, P_C1, P_C2, P_D2, P
--- Clinton/v3/PATHS.h
+++ Clinton/v3/PATHS.h
@@ -5,7 +5,7 @@
 #define P_A2 Vector2d(1250, 250)
 #define P_A3 Vector2d(1750, 250)
 
-#define P_B0 Vector2d(250, 750)
+#define P_B0 Vector2d(250, 750-124)
 #define P_B1 Vector2d(750, 750)
 #define P_B2 Vector2d(1250, 750)
 #define P_B3 Vector2d(1750, 750)
