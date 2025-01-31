032c311d4186952595cbeacebb138ff0e45b3354
Kellen Yu <kyu50@stuy.edu>
2025-01-31 16:44:40 -0500

    update

--- Clinton/v3/PATH0.h
+++ Clinton/v3/PATH0.h
@@ -1,7 +1,7 @@
 #include "PATHS.h"
-#define PATH0_SIZE 8
+#define PATH0_SIZE 0
 
-#define PATH0 {EDGE_2, P_A2, P_A3, P_B3, P_B2, P_D2, P_D0, P_B0}
+#define PATH0 {}
 //Starting point and heading
 
 #define TARGET_TIME 62 //offset1
